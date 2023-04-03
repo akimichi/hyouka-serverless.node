d79b84e165b0155cbc6dc6c585e561473b2437c3
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2023-04-03 21:47:34 +0900

    lisp式 1 の評価に成功した

--- handler.js
+++ handler.js
@@ -6,26 +6,28 @@ module.exports.evaluate = async (event, context, callback) => {
     Syntax = Hyouka.Syntax,
     Semantics = Hyouka.Semantics,
     Interpreter = Hyouka.Interpreter;
+  const Evaluator = Interpreter(Syntax.expression, Semantics.evaluator);
 
   const { Maybe, Cont, IO } = Hyouka.Monad
 
-  const body = JSON.parse(event.body)
+  // const body = JSON.parse(event.body)
+  const body = event.body
   // console.log("body", body)
-  if (typeof data.code !== 'string') {
+  if (typeof body.code !== 'string') {
     console.error('Validation Failed');
     callback(null, {
       statusCode: 400,
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(
-        {
+      body: JSON.stringify({
           message: "Validation Error",
-          input: event,
-        })
+          input: event
+      })
     });
     return;
   } else {
     const environment = Env.prelude()
-    Maybe.match(Cont.eval(Evaluator(environment)(data.code)),{
+    // Maybe.match(Semantics.evaluate(body.code)(environment),{
+    Maybe.match(Cont.eval(Evaluator(environment)(body.code)),{
       nothing: (message) => {
         callback(null, {
           statusCode: 400,
@@ -40,7 +42,7 @@ module.exports.evaluate = async (event, context, callback) => {
       },
       just: (value) => {
         callback(null, {
-          statusCode: 400,
+          statusCode: 200,
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(
             {
