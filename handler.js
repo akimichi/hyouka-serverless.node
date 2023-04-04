774e63ace3edabb03c0051d6b95dc7a6b6549601
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2023-04-04 13:54:31 +0900

    API Gateway 上でのテストに成功

--- handler.js
+++ handler.js
@@ -1,18 +1,18 @@
 'use strict';
+const Hyouka = require('hyouka.js');
+const Env = Hyouka.Env,
+  Syntax = Hyouka.Syntax,
+  Semantics = Hyouka.Semantics,
+  Interpreter = Hyouka.Interpreter;
+const Evaluator = Interpreter(Syntax.expression, Semantics.evaluator);
+
+const { Maybe, Cont, IO } = Hyouka.Monad
 
 module.exports.evaluate = async (event, context, callback) => {
-  const Hyouka = require('hyouka.js');
-  const Env = Hyouka.Env,
-    Syntax = Hyouka.Syntax,
-    Semantics = Hyouka.Semantics,
-    Interpreter = Hyouka.Interpreter;
-  const Evaluator = Interpreter(Syntax.expression, Semantics.evaluator);
-
-  const { Maybe, Cont, IO } = Hyouka.Monad
-
   // const body = JSON.parse(event.body)
-  const body = event.body
-  // console.log("body", body)
+  const body = JSON.parse(event.body)
+  console.log("body", body)
+
   if (typeof body.code !== 'string') {
     console.error('Validation Failed');
     callback(null, {
@@ -25,34 +25,30 @@ module.exports.evaluate = async (event, context, callback) => {
     });
     return;
   } else {
+    console.log("body.code", body.code)
     const environment = Env.prelude()
     Maybe.match(Cont.eval(Evaluator(environment)(body.code)),{
       nothing: (message) => {
+        console.log("nothing", message)
         callback(null, {
           statusCode: 400,
           headers: { 'Content-Type': 'application/json' },
-          body: {
+          body: JSON.stringify({
             message,
             input: event,
-          }
+          })
         });
-        return
       },
       just: (value) => {
+        console.log("just", value)
         callback(null, {
           statusCode: 200,
           headers: { 'Content-Type': 'application/json' },
-          body: {
-            answer: value,
-            input: event,
-          }
-          // body: JSON.stringify(
-          //   {
-          //     message: value,
-          //     input: event,
-          //   })
+          body: JSON.stringify(
+            {
+              answer: value
+            })
         });
-        return
       }
     })
   }
